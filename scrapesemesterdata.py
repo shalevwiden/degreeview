a672d5301340cf89e07c73129affc7fb13d54233
Shalev Widen <shalevwiden@gmail.com>
2025-06-30 12:52:44 -0500

    scrapesemesterdata update
    Nailed navigating the trs to get a result that includes the semester headings.

--- scrapesemesterdata.py
+++ scrapesemesterdata.py
@@ -44,10 +44,8 @@ def getallcourses_splitbysemester(suggcourse_link):
     # can do multiple find alls hmmm
 
     # I think I can do all the logic in this function turns out.
-    # get only the even and odd trs for sure
+    # get only the even and odd trs for sure - this gets the semester headings too
     unfiltered_trs=course_soup.find_all('tr',attrs={"class":["even","odd"]})
-    # use areaheader later
-    unfiltered_trs=[tr for tr in unfiltered_trs if "areaheader" not in tr.get("class",[])]
 
     trs=[]
     # filter to remove orclass tds and add those to trs list. This is kinda complex.
@@ -57,106 +55,116 @@ def getallcourses_splitbysemester(suggcourse_link):
         if all("orclass" not in td.get("class", []) for td in tds):
             trs.append(tr)
     
+    semestercount=0
     # for every grouping of names, code, hours, status
+    # this is the BIG FOR LOOP
     for row in trs:
-        tds=row.find_all('td')
-        if len(tds)==3:
+        if  "areaheader" not in row.get("class",[]):
+            tds=row.find_all('td')
+            if len(tds)==3:
 
-            coursename=tds[1]
-           
-            coursename_and_type=coursename.get_text().split('(')
-    # only keep the first bit of the sentence, the useful info
-            coursename=coursename_and_type[0].strip()
-
-            coursetype=coursename_and_type[1].split(')')[0]
-
-            firsttd=tds[0]
-
-            # looks for the a's in the current td list
-            coursecodes=firsttd.find_all('a',attrs={"class": ["bubblelink", "code"]})
-            # print(f'len of coursecodes is {len(coursecodes)}\n')
-            for coursecode in coursecodes:
-                # defines coursecode here although tricky. Remove Unicode space.
-                coursecode=coursecode.text.replace('\xa0',' ')
-
+                coursename=tds[1]
             
+                coursename_and_type=coursename.get_text().split('(')
+        # only keep the first bit of the sentence, the useful info
+                coursename=coursename_and_type[0].strip()
 
-            # turns out coursecode is a string
+                coursetype=coursename_and_type[1].split(')')[0]
 
-            # get the status from the number stuff
+                firsttd=tds[0]
 
-            lowerdivstatus="Lower Division"
-            upperdivstatus="Upper Division"
-            status=''
-            # check if theres a number in it
-            if any(char.isdigit() for char in coursecode):
-                coursenum=coursecode.split(' ')[-1][1:3]
-                if int(coursenum)>=20:
-                    status=upperdivstatus
-                else:
-                    status=lowerdivstatus
-            else:
-                if "lower" in coursecode.lower():
-                    status=lowerdivstatus
-                else:
-                    status=upperdivstatus
+                # looks for the a's in the current td list
+                coursecodes=firsttd.find_all('a',attrs={"class": ["bubblelink", "code"]})
+                # print(f'len of coursecodes is {len(coursecodes)}\n')
+                for coursecode in coursecodes:
+                    # defines coursecode here although tricky. Remove Unicode space.
+                    coursecode=coursecode.text.replace('\xa0',' ')
 
-            
+                
 
-            thirdtd=tds[2]
-            coursehours='na'
-            # gotta compare it to a list, since .get returns a list, since there can be multiple classes
+                # turns out coursecode is a string
 
-            # .get returns many diferent attributes - very useful
-            if "hourscol" in thirdtd.get("class",[]):
-                coursehours=thirdtd.get_text()
-            semesterdictionary[coursename]=[coursecode, coursehours, status, f'({coursetype})']
+                # get the status from the number stuff
 
-            # get and add the upper and lower division stuff
-            
+                lowerdivstatus="Lower Division"
+                upperdivstatus="Upper Division"
+                status=''
+                # check if theres a number in it
+                if any(char.isdigit() for char in coursecode):
+                    coursenum=coursecode.split(' ')[-1][1:3]
+                    if int(coursenum)>=20:
+                        status=upperdivstatus
+                    else:
+                        status=lowerdivstatus
+              
 
-            # the length of the td is 2 when theres no course code
-            # this happens when its an unspecified course like upper division course
+                
+
+                thirdtd=tds[2]
+                coursehours='na'
+                # gotta compare it to a list, since .get returns a list, since there can be multiple classes
+
+                # .get returns many diferent attributes - very useful
+                if "hourscol" in thirdtd.get("class",[]):
+                    coursehours=thirdtd.get_text()
+                semesterdictionary[coursename]=[coursecode, coursehours, status, f'({coursetype})']
+
+                # get and add the upper and lower division stuff
+                
+
+                # the length of the td is 2 when theres no course code
+                # this happens when its an unspecified course like upper division course
+            elif len(tds)==2:
 
 
-        elif len(tds)==2:
+                coursename=tds[0]
 
-            coursename=tds[0]
-
-                    
                         
-            coursename_and_type=coursename.get_text().split('(')
-            # only keep the first bit of the sentence, the useful info
-            coursename=coursename_and_type[0].strip()
-            coursetype=coursename_and_type[1].split(')')[0]
+                            
+                coursename_and_type=coursename.get_text().split('(')
+                # only keep the first bit of the sentence, the useful info
+                coursename=coursename_and_type[0].strip()
+                coursetype=coursename_and_type[1].split(')')[0]
 
 
 
 
-            # getting and assigning the status variable
-            lowerdivstatus="Lower Division"
-            upperdivstatus="Upper Division"
-            status=''
-            if "lower" in coursename.lower():
-                status=lowerdivstatus
-            else:
-                status=upperdivstatus
+                # getting and assigning the status variable
+                lowerdivstatus="Lower Division"
+                upperdivstatus="Upper Division"
+                status=''
+                if "lower" in coursename.lower():
+                    status=lowerdivstatus
+                elif "upper" in coursename.lower():
+                    status=upperdivstatus
+                else:
+                    # if neither upper or lower leave it blank
+                    status=''
 
-            secondtd=tds[1]
+                secondtd=tds[1]
 
 
-            # extra assurance to check the class
-            if "hourscol" in secondtd.get("class",[]):
-                coursehours=secondtd.get_text()
+                # extra assurance to check the class
+                if "hourscol" in secondtd.get("class",[]):
+                    coursehours=secondtd.get_text()
+                
+                # now adding to the dictionary logic
+                if coursename not in semesterdictionary:
+                    # removed it from "no course code" to just an empty string
+                    semesterdictionary[coursename]=[['', coursehours,status, f'({coursetype})']]
+                else:
+                    semesterdictionary[coursename].append(['',coursehours,status, f'({coursetype})'])
+
+                    # get upper div by textin coursename for this one since no coursecode
+                
+
+        elif  "areaheader" in row.get("class",[]):
+            semestercount+=1
+            print('\n there was an areaheader\n')
+
+            semesternum=row.find('span',{"class":["courselistcomment","areaheader"]}).get_text()
+            semesterdictionary[f'Semester {semestercount}']=semesternum
             
-            # now adding to the dictionary logic
-            if coursename not in semesterdictionary:
-                # removed it from "no course code" to just an empty string
-                semesterdictionary[coursename]=[['', coursehours,status, f'({coursetype})']]
-            else:
-                semesterdictionary[coursename].append(['',coursehours,status, f'({coursetype})'])
-
-                # get upper div by textin coursename for this one since no coursecode
 
     return semesterdictionary
 
