84e30e93604b5a983c76f8a45a5f873805194923
Shalev Widen <shalevwiden@gmail.com>
2025-07-04 22:03:28 -0500

    worked on semesterdata new makewebsite file might just do it in a new folder

---
+++ makewebsite.py
@@ -0,0 +1,9 @@
+import os
+
+from theassetcontainment import theasset
+
+websitefolder_path='/Users/shalevwiden/Downloads/Projects/degreeviewwebsite'
+index_file='/Users/shalevwiden/Downloads/Projects/degreeviewwebsite/index.html'
+
+# hmm so I'll need to link to each folder, and to the many elements in each folder too...
+
